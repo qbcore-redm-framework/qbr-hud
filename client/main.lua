e83ecf3d4878f078dcedaa4417aeba6821fe3a13
DosKit <56743692+DosKit@users.noreply.github.com>
2023-06-17 03:01:25 -0400

    Update main.lua
    Cleaned Up as Well as Converted the isLoggedIn to StateBagChangeHandler to prevent any deserializing as well as helps with performance on the thread.

--- client/main.lua
+++ client/main.lua
@@ -1,119 +1,70 @@
-
 local speed = 0.0
-local radarActive = false
 local stress = 0
 local hunger = 100
 local thirst = 100
 local cashAmount = 0
 local bankAmount = 0
+local pid = PlayerId()
 local isLoggedIn = false
+local sid = GetPlayerServerId(pid)
 
--- Events
+----------------------------------------------------------------------------
+---- FUNCTIONS
+----------------------------------------------------------------------------
 
-RegisterNetEvent('QBCore:Client:OnPlayerUnload')
-AddEventHandler('QBCore:Client:OnPlayerUnload', function()
-    isLoggedIn = false
+local function GetShakeIntensity(stresslevel)
+    local retval = 0.05
+    for k, v in pairs(Config.Intensity['shake']) do
+        if stresslevel >= v.min and stresslevel <= v.max then
+            retval = v.intensity
+            break
+        end
+    end
+    return retval
+end
+
+local function GetEffectInterval(stresslevel)
+    local retval = 60000
+    for k, v in pairs(Config.EffectInterval) do
+        if stresslevel >= v.min and stresslevel <= v.max then
+            retval = v.timeout
+            break
+        end
+    end
+    return retval
+end
+
+----------------------------------------------------------------------------
+---- EVENTS & HANDLERS
+----------------------------------------------------------------------------
+
+AddStateBagChangeHandler('isLoggedIn', ('player:%s'):format(sid), function(_, _, value)
+    isLoggedIn = value
 end)
 
-RegisterNetEvent('QBCore:Client:OnPlayerLoaded')
-AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
-    isLoggedIn = true
+RegisterNetEvent('hud:client:UpdateNeeds', function(newHunger, newThirst) -- Triggered in qbr-core
+    hunger, thirst = newHunger, newThirst
 end)
 
-RegisterNetEvent('hud:client:UpdateNeeds') -- Triggered in qbr-core
-AddEventHandler('hud:client:UpdateNeeds', function(newHunger, newThirst)
-    hunger = newHunger
+RegisterNetEvent('hud:client:UpdateThirst', function(newThirst) -- Triggered in qbr-core
     thirst = newThirst
 end)
 
-RegisterNetEvent('hud:client:UpdateThirst') -- Triggered in qbr-core
-AddEventHandler('hud:client:UpdateThirst', function(newThirst)
-    thirst = newThirst
-end)
-
-RegisterNetEvent('hud:client:UpdateStress') -- Add this event with adding stress elsewhere
-AddEventHandler('hud:client:UpdateStress', function(newStress)
+RegisterNetEvent('hud:client:UpdateStress', function(newStress) -- Add this event with adding stress elsewhere
     stress = newStress
 end)
 
--- Player HUD
-Citizen.CreateThread(function()
-    while true do
-        Wait(500)
-        if LocalPlayer.state['isLoggedIn'] then
-            local show = true
-            local player = PlayerPedId()
-            local playerid = PlayerId()
-            if IsPauseMenuActive() then
-                show = false
-            end	
-            SendNUIMessage({
-                action = 'hudtick',
-                show = show,
-                health = GetEntityHealth(player) / 3, -- health in red dead is 300 so dividing by 3 makes it 100 here
-                armor = Citizen.InvokeNative(0x2CE311A7, player),
-                thirst = thirst,
-                hunger = hunger,
-                stress = stress,
-				voice = Citizen.InvokeNative(0x33EEF97F, playerid)
-            })
-        else
-            SendNUIMessage({
-                action = 'hudtick',
-                show = false,
-            })
-        end
-    end
-end)
-
-Citizen.CreateThread(function()
-    while true do
-        Wait(1)
-          if IsPedOnMount(PlayerPedId()) or IsPedOnVehicle(PlayerPedId()) then
-            if Config.MounttMinimap then
-                if Config.MountCompass then
-                    SetMinimapType(3)
-                else
-                    SetMinimapType(1)
-                end
-            else
-                SetMinimapType(0)
-            end
-          else
-            if not Config.OnFootMinimap then
-              SetMinimapType(0)
-              Wait(2000)
-            else
-                if Config.OnFootCompass then
-                    SetMinimapType(3)
-                else
-                    SetMinimapType(1)
-                end
-            end
-          end
-     end
-  end)
 -- Money HUD
 
-RegisterNetEvent('hud:client:ShowAccounts')
-AddEventHandler('hud:client:ShowAccounts', function(type, amount)
+RegisterNetEvent('hud:client:ShowAccounts', function(type, amount)
     if type == 'cash' then
-        SendNUIMessage({
-            action = 'show',
-            type = 'cash',
-            cash = amount,
-        })
+        SendNUIMessage({action = 'show', type = 'cash', cash = amount})
     else
-        SendNUIMessage({
-            action = 'show',
-            type = 'bank',
-            bank = amount,
-        })
+        SendNUIMessage({action = 'show', type = 'bank', bank = amount})
     end
 end)
 
-RegisterNetEvent('hud:client:OnMoneyChange')
-AddEventHandler('hud:client:OnMoneyChange', function(type, amount, isMinus)
+RegisterNetEvent('hud:client:OnMoneyChange', function(type, amount, isMinus)
     exports['qbr-core']:GetPlayerData(function(PlayerData)
         cashAmount = PlayerData.money['cash']
         bankAmount = PlayerData.money['bank']
@@ -128,11 +79,69 @@ AddEventHandler('hud:client:OnMoneyChange', function(type, amount, isMinus)
     })
 end)
 
+----------------------------------------------------------------------------
+---- THREADS
+----------------------------------------------------------------------------
+
+-- Player HUD
+CreateThread(function()
+    while true do
+        Wait(500)
+        if isLoggedIn then
+            local show = true
+            local player = PlayerPedId()
+            if IsPauseMenuActive() then
+                show = false
+            end	
+            SendNUIMessage({
+                action = 'hudtick',
+                show = show,
+                health = GetEntityHealth(player) / 3, -- health in red dead is 300 so dividing by 3 makes it 100 here
+                armor = Citizen.InvokeNative(0x2CE311A7, player),
+                thirst = thirst,
+                hunger = hunger,
+                stress = stress,
+				voice = Citizen.InvokeNative(0x33EEF97F, pid)
+            })
+        else
+            SendNUIMessage({action = 'hudtick', show = false})
+        end
+    end
+end)
+
+CreateThread(function()
+    while true do
+        Wait(1)
+        if IsPedOnMount(PlayerPedId()) or IsPedOnVehicle(PlayerPedId()) then
+            if Config.MounttMinimap then
+                if Config.MountCompass then
+                    SetMinimapType(3)
+                else
+                    SetMinimapType(1)
+                end
+            else
+                SetMinimapType(0)
+            end
+        else
+            if not Config.OnFootMinimap then
+              SetMinimapType(0)
+              Wait(2000)
+            else
+                if Config.OnFootCompass then
+                    SetMinimapType(3)
+                else
+                    SetMinimapType(1)
+                end
+            end
+        end
+    end
+end)
+
 -- Stress Gain
 
-Citizen.CreateThread(function() -- Speeding
+CreateThread(function() -- Speeding
     while true do
-        if QBCore ~= nil --[[ and isLoggedIn ]] then
+        if isLoggedIn then
             local ped = PlayerPedId()
             if IsPedInAnyVehicle(ped, false) then
                 speed = GetEntitySpeed(GetVehiclePedIsIn(ped, false)) * 2.237 --mph
@@ -141,29 +150,29 @@ Citizen.CreateThread(function() -- Speeding
                 end
             end
         end
-        Citizen.Wait(20000)
+        Wait(20000)
     end
 end)
 
-Citizen.CreateThread(function() -- Shooting
+CreateThread(function() -- Shooting
     while true do
-        if QBCore ~= nil --[[ and isLoggedIn ]] then
+        if isLoggedIn then
             if IsPedShooting(PlayerPedId()) then
                 if math.random() < Config.StressChance then
                     TriggerServerEvent('hud:server:GainStress', math.random(1, 3))
                 end
             end
         end
-        Citizen.Wait(6)
+        Wait(6)
     end
 end)
 
 -- Stress Screen Effects
 
-Citizen.CreateThread(function()
+CreateThread(function()
     while true do
         local ped = PlayerPedId()
-        local Wait = GetEffectInterval(stress)
+        local interval = GetEffectInterval(stress)
         if stress >= 100 then
             local ShakeIntensity = GetShakeIntensity(stress)
             local FallRepeat = math.random(2, 4)
@@ -172,15 +181,14 @@ Citizen.CreateThread(function()
             SetFlash(0, 0, 500, 3000, 500)
 
             if not IsPedRagdoll(ped) and IsPedOnFoot(ped) and not IsPedSwimming(ped) then
-                local player = PlayerPedId()
-                SetPedToRagdollWithFall(player, RagdollTimeout, RagdollTimeout, 1, GetEntityForwardVector(player), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+                SetPedToRagdollWithFall(ped, RagdollTimeout, RagdollTimeout, 1, GetEntityForwardVector(ped), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
             end
 
-            Citizen.Wait(500)
+            Wait(500)
             for i = 1, FallRepeat, 1 do
-                Citizen.Wait(750)
+                Wait(750)
                 DoScreenFadeOut(200)
-                Citizen.Wait(1000)
+                Wait(1000)
                 DoScreenFadeIn(200)
                 ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', ShakeIntensity)
                 SetFlash(0, 0, 200, 750, 200)
@@ -190,28 +198,6 @@ Citizen.CreateThread(function()
             ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', ShakeIntensity)
             SetFlash(0, 0, 500, 2500, 500)
         end
-        Citizen.Wait(Wait)
+        Wait(interval)
     end
 end)
-
-function GetShakeIntensity(stresslevel)
-    local retval = 0.05
-    for k, v in pairs(Config.Intensity['shake']) do
-        if stresslevel >= v.min and stresslevel <= v.max then
-            retval = v.intensity
-            break
-        end
-    end
-    return retval
-end
-
-function GetEffectInterval(stresslevel)
-    local retval = 60000
-    for k, v in pairs(Config.EffectInterval) do
-        if stresslevel >= v.min and stresslevel <= v.max then
-            retval = v.timeout
-            break
-        end
-    end
-    return retval
-end
